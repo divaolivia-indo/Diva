b8462025deb9c8110727821bfbce2e6ba7202d42
divaolivia-indo <divaolivia37@gmail.com>
2025-04-28 10:16:30 +0700

    first commit

--- app.py
+++ app.py
@@ -6,6 +6,3 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 @app.route('/')
 def home():
     return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
