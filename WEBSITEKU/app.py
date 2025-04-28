3f764cca1278505ff18b996aee732e388a45c53e
divaolivia-indo <divaolivia37@gmail.com>
2025-04-28 19:27:07 +0700

    mywebsite

---
+++ WEBSITEKU/app.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__, static_folder='static', template_folder='templates')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
